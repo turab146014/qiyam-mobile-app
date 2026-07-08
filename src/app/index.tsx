5cc5e87eaeb9de499c5ba585aec82b78e7587f89
Syed Turab Haider Naqvi <turabhaider621@gmail.com>
2026-07-08 11:46:54 +0500

    chore: setup qiyam app base configuration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold text-green-700">
+    <View className="flex-1 items-center justify-center bg-[#fefefd]">
+      <Text className="text-2xl font-bold text-[#36a4af]">
         Qiyam App
       </Text>
     </View>
