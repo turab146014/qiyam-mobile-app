fcd16eacbf5d648b8ba940cae68e628f7e977a51
Syed Turab Haider Naqvi <turabhaider621@gmail.com>
2026-07-23 16:50:33 +0500

    chore: complete appwrite setup for majlis alerts

---
+++ src/services/appwrite.ts
@@ -0,0 +1,19 @@
+import "react-native-url-polyfill/auto";
+import { Client, TablesDB } from "react-native-appwrite";
+
+export const appwriteConfig = {
+  endpoint: "https://syd.cloud.appwrite.io/v1",
+  projectId: "6a61f72e003c3258ed6a",
+  platform: "com.fourteenlabs.qiyamapp",
+  databaseId: "qiyam_db",
+  majlisTableId: "majlis",
+};
+
+const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+export const tablesDB = new TablesDB(client);
